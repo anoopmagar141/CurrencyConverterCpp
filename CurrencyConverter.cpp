a95a6a8b0600dc7ba0fd95dcdd610f0d7fc28625
[Anup  Magar] <am6956275@gmail.com>
2025-04-03 23:10:31 +0545

    main function

--- CurrencyConverter.cpp
+++ CurrencyConverter.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 #include <unordered_map>
 #include <stdexcept>
+using namespace std;
+int main() {
+   
+    string fromCurrency, toCurrency;
+    double amount;
+
+    cout << "Welcome to Currency Converter!\n";
+    cout << "Enter source currency (e.g., USD, EUR, INR): ";
+    cin >> fromCurrency;
+    cout << "Enter target currency (e.g., USD, EUR, INR): ";
+    cin >> toCurrency;
+    cout << "Enter amount: ";
+    cin >> amount;
+
+    return 0;
+}
